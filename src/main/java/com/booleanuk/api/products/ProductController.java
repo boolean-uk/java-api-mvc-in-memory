e6ad0be27b07493cf8347f52e1c136f935077fce
iliask796 <iliaskal7@gmail.com>
2023-09-27 17:35:40 +0300

    Added Error Codes (Extension Done)

--- src/main/java/com/booleanuk/api/products/ProductController.java
+++ src/main/java/com/booleanuk/api/products/ProductController.java
@@ -1,8 +1,11 @@
 package com.booleanuk.api.products;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.management.InstanceAlreadyExistsException;
 import java.util.ArrayList;
 
 @RestController
@@ -15,29 +18,60 @@ public class ProductController {
     }
 
     @GetMapping
-    public ArrayList<Product> getAllProducts(){
-        return this.productRepo.findAll();
+    public ArrayList<Product> getAllProducts(@RequestParam (required = false) String category){
+        ArrayList<Product> selectedProducts;
+        if (category == null){
+            selectedProducts = this.productRepo.findAll();
+        } else {
+            selectedProducts = this.productRepo.findByCategory(category);
+        }
+        if (selectedProducts == null && category == null){
+            throw new ResponseStatusException(HttpStatusCode.valueOf(404),"No products were found.");
+        } else if (selectedProducts == null){
+            throw new ResponseStatusException(HttpStatusCode.valueOf(404),"No products of provided category were found.");
+        }
+        return selectedProducts;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Product postProduct(@RequestBody Product product){
-        return this.productRepo.create(product);
+        Product toBeCreated = this.productRepo.create(product);
+        if (toBeCreated == null){
+            throw new ResponseStatusException(HttpStatusCode.valueOf(400),"Product with provided name already exists.");
+        }
+        return toBeCreated;
     }
 
     @GetMapping("/{id}")
     public Product getProduct(@PathVariable int id){
-        return this.productRepo.find(id);
+        Product toBeFound = this.productRepo.find(id);
+        if (toBeFound == null){
+            throw new ResponseStatusException(HttpStatusCode.valueOf(404),"Product not found.");
+        }
+        return toBeFound;
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.CREATED)
     public Product putProduct(@PathVariable int id, @RequestBody Product product){
-        return this.productRepo.update(id, product);
+        try {
+            Product toBeUpdated =  this.productRepo.update(id, product);
+            if (toBeUpdated == null){
+                throw new ResponseStatusException(HttpStatusCode.valueOf(404),"Product not found.");
+            }
+            return toBeUpdated;
+        } catch (InstanceAlreadyExistsException e){
+            throw new ResponseStatusException(HttpStatusCode.valueOf(400), e.getMessage());
+        }
     }
 
     @DeleteMapping("/{id}")
     public Product deleteProduct(@PathVariable int id){
-        return this.productRepo.delete(id);
+        Product toBeDeleted = this.productRepo.delete(id);
+        if (toBeDeleted == null){
+            throw new ResponseStatusException(HttpStatusCode.valueOf(404),"Product not found.");
+        }
+        return toBeDeleted;
     }
 }
--- src/main/java/com/booleanuk/api/products/ProductRepository.java
+++ src/main/java/com/booleanuk/api/products/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.booleanuk.api.products;
 
+import javax.management.InstanceAlreadyExistsException;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class ProductRepository {
     static int productID = 1;
@@ -14,6 +16,12 @@ public class ProductRepository {
         return this.products.isEmpty()? null: this.products;
     }
 
+    public ArrayList<Product> findByCategory(String category){
+        return this.products.stream()
+                .filter(thisProduct -> thisProduct.getCategory().equals(category))
+                .collect(Collectors.collectingAndThen(Collectors.toCollection(ArrayList<Product>::new), c -> !c.isEmpty()? c: null));
+    }
+
     public Product find(int id){
         return this.products.stream()
                 .filter(thisProduct -> thisProduct.getId() == id)
@@ -22,20 +30,35 @@ public class ProductRepository {
     }
 
     public Product create(Product product){
-        Product toBeCreated = new Product(productID++, product.getName(), product.getCategory(), product.getPrice());
-        this.products.add(toBeCreated);
-        return toBeCreated;
+        Product existingProduct = this.products.stream()
+                .filter(thisProduct -> thisProduct.getName().equals(product.getName()))
+                .findAny()
+                .orElse(null);
+        if (existingProduct == null){
+            Product toBeCreated = new Product(productID++, product.getName(), product.getCategory(), product.getPrice());
+            this.products.add(toBeCreated);
+            return toBeCreated;
+        }
+        return null;
     }
 
-    public Product update(int id, Product product){
+    public Product update(int id, Product product) throws InstanceAlreadyExistsException{
         Product toBeUpdated = this.products.stream()
                 .filter(thisProduct -> thisProduct.getId() == id)
                 .findAny()
                 .orElse(null);
         if (toBeUpdated != null){
-            toBeUpdated.setName(product.getName());
-            toBeUpdated.setCategory(product.getCategory());
-            toBeUpdated.setPrice(product.getPrice());
+            Product existingProduct = this.products.stream()
+                    .filter(thisProduct -> thisProduct.getName().equals(product.getName()))
+                    .findAny()
+                    .orElse(null);
+            if (existingProduct == null){
+                toBeUpdated.setName(product.getName());
+                toBeUpdated.setCategory(product.getCategory());
+                toBeUpdated.setPrice(product.getPrice());
+            } else {
+                throw new InstanceAlreadyExistsException("Product with provided name already exists.");
+            }
         }
         return toBeUpdated;
     }
