3ae8818a3db0ebfb22813e26696ba987676a05af
george georgopoulos <62259101+ggeorgopoulos1998@users.noreply.github.com>
2023-10-02 10:43:58 +0300

    core is completed

--- src/main/java/com/booleanuk/api/bagels/BagelRepository.java
+++ src/main/java/com/booleanuk/api/bagels/BagelRepository.java
@@ -1,25 +1,40 @@
 package com.booleanuk.api.bagels;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BagelRepository {
     private int idCounter = 1;
-    private List<Bagel> data = new ArrayList<>();
+    private Map<Integer, Bagel> bagels = new HashMap<>();
 
-    public void create(String type, int price) {
-        Bagel bagel = new Bagel(this.idCounter++, type, price);
-        this.data.add(bagel);
+    public Bagel create(String type, int price) {
+        Bagel bagel = new Bagel(idCounter, type, price);
+        bagels.put(idCounter, bagel);
+        idCounter++;
+        return bagel;
     }
 
     public List<Bagel> findAll() {
-        return this.data;
+        return new ArrayList<>(bagels.values());
     }
 
     public Bagel find(int id) {
-        return this.data.stream()
-                .filter(bagel -> bagel.getId() == id)
-                .findFirst()
-                .orElseThrow();
+        return bagels.get(id);
+    }
+
+    public Bagel update(int id, String type, int price) {
+        Bagel bagel = bagels.get(id);
+        if (bagel != null) {
+            bagel.setType(type);
+            bagel.setPrice(price);
+            return bagel;
+        }
+        return null;
+    }
+
+    public Bagel delete(int id) {
+        return bagels.remove(id);
     }
 }
