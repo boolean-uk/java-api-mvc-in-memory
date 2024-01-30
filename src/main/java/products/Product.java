9eaeb6a2eda7b6fab060d2d90869f68a29802c6e
Alexander <alexander.ellnestam@gmail.com>
2024-01-30 16:25:44 +0100

    Done with core and extensions

--- src/main/java/products/Product.java
+++ src/main/java/products/Product.java
@@ -1,4 +1,55 @@
 package products;
 
+
+//Define object product in this class
 public class Product {
+
+    private String name;
+    private String category;
+    private int price;
+
+    private int id;
+
+    private int nextId = 0;
+
+    public Product(String name, String category, int price){
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.id = nextId++;
+    }
+
+
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- src/main/java/products/ProductRepository.java
+++ src/main/java/products/ProductRepository.java
@@ -1,4 +1,65 @@
 package products;
 
+import java.util.ArrayList;
+import java.util.List;
+
+//Store data, and create methods to retrieve it
 public class ProductRepository {
+
+    private ArrayList<Product> products;
+    public ProductRepository(){
+        this.products = new ArrayList<>();
+    }
+
+
+
+    public List<Product> getAll(){
+        return products;
+    }
+
+    public List<Product> getAll(String category){
+        return products.stream().filter(product -> product.getCategory().equalsIgnoreCase(category)).toList();
+    }
+
+    public Product getOne(String name){
+        for (Product product : products){
+            if (product.getName().equals(name)){
+                return product;
+            }
+        }
+        return null;
+    }
+
+
+    public Product getOne(int id){
+        return products.stream().filter(product -> product.getId() == id).findFirst().orElse(null);
+
+    }
+    public Product create(Product product){
+        if (products.stream().noneMatch(product1 -> product1.getName().equals(product.getName()))){
+            products.add(product);
+            return product;
+        }
+        return null;
+    }
+
+    public Product delete(int id){
+        Product product = products.stream().filter(product1 -> product1.getId() == id).findFirst().orElse(null);
+        if (product != null){
+            products.remove(product);
+        }
+        return product;
+    }
+
+    public Product update(int id, Product updProd){
+        Product product = products.stream().filter(product1 -> product1.getId() == id).findFirst().orElse(null);
+        if (product != null){
+            product.setCategory(updProd.getCategory());
+            product.setPrice(updProd.getPrice());
+            product.setName(updProd.getName());
+        }
+        return product;
+    }
+
+
 }
