101dfeadf804dc5ec6880c02f6a7241e7ec1539d
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-01-30 14:13:30 +0100

    Add extension

--- src/main/java/com/booleanuk/api/bagels/model/Product.java
+++ src/main/java/com/booleanuk/api/bagels/model/Product.java
@@ -1,11 +1,8 @@
 package com.booleanuk.api.bagels.model;
 
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 @Getter
 @Setter
 public class Product {
--- src/main/java/com/booleanuk/api/bagels/model/ProductRepository.java
+++ src/main/java/com/booleanuk/api/bagels/model/ProductRepository.java
@@ -13,10 +13,16 @@ public class ProductRepository {
     List<Product> products = new ArrayList<>();
 
     public List<Product> getAll() {
+        if(products.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
         return this.products;
     }
 
     public Product postOne(Product product) {
+        if(this.products.contains(product)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
         this.products.add(product);
         return product;
     }
@@ -32,7 +38,11 @@ public class ProductRepository {
     }
 
     public Product putOne(int id, Product product) {
+
         for(Product prod : this.products) {
+            if(prod.getName().equalsIgnoreCase(product.getName())) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
             if(prod.getId() == id) {
                 prod.setName(product.getName());
                 prod.setCategory(product.getCategory());
