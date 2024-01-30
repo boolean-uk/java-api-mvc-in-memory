63e583558469706a9a136ed619aaba60d23d1541
ThomasKvam <thomas.e.h.kvam@hotmail.no>
2024-01-30 12:54:12 +0100

    Added all required mappings

--- src/main/java/com/booleanuk/api/products/ProductController.java
+++ src/main/java/com/booleanuk/api/products/ProductController.java
@@ -1,7 +1,10 @@
 package com.booleanuk.api.products;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("products")
@@ -10,6 +13,37 @@ public class ProductController {
 
     public ProductController(){
         this.theProducts = new ProductRepository();
+    }
+
+    @GetMapping
+    public ArrayList<Product> getAll() {
+        return this.theProducts.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product getOne(@PathVariable(name = "id") int id) {
+        Product product = this.theProducts.getOne(id);
+        if (product == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Not found");
+        }
+        return product;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product createAuthor(@RequestBody Product author){
+        return this.theProducts.create(author);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product editAuthor(@PathVariable(name = "id") int id, @RequestBody Product author){
+        return this.theProducts.edit(id, author);
+    }
+
+    @DeleteMapping("/{id}")
+    public Product deleteAuthor(@PathVariable(name="id") int id){
+        return this.theProducts.delete(id);
     }
 
 }
