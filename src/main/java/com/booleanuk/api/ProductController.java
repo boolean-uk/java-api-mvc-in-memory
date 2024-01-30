b25600e5f338271cc3bc16be1957c540d63de3b7
Maha Riazi <maha.riazi@hotmail.no>
2024-01-30 12:47:57 +0100

    core check

--- src/main/java/com/booleanuk/api/ProductController.java
+++ src/main/java/com/booleanuk/api/ProductController.java
@@ -1,4 +1,58 @@
 package com.booleanuk.api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("products")
 public class ProductController {
+    private ProductRepository products;
+
+    public ProductController(){
+        products = new ProductRepository();
+    }
+
+    @GetMapping
+    public List<Product> getAll(){
+        return products.getProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Product getOne(@PathVariable int id){
+        return products.getProduct(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product create(@RequestBody Product product){
+        products.addProduct(product);
+
+        return products.getProduct(product.getId());
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product update(@PathVariable int id, @RequestBody Product updated){
+        Product product = products.getProduct(id);
+
+        if (product != null){
+            product.setName(updated.getName());
+            product.setCategory(updated.getCategory());
+            product.setPrice(updated.getPrice());
+
+            return products.getProduct(id);
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public Product delete(@PathVariable int id){
+        if (products.getProduct(id) != null){
+            return products.deleteProduct(id);
+        }
+
+        return null;
+    }
 }
--- src/main/java/com/booleanuk/api/ProductRepository.java
+++ src/main/java/com/booleanuk/api/ProductRepository.java
@@ -1,4 +1,35 @@
 package com.booleanuk.api;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductRepository {
+    private List<Product> products;
+
+    public ProductRepository(){
+        products = new ArrayList<>();
+    }
+
+    public List<Product> getProducts(){
+        return products;
+    }
+
+    public Product getProduct(int id){
+        for (Product product : products){
+            if (product.getId() == id){
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public void addProduct(Product product){
+        products.add(product);
+    }
+
+    public Product deleteProduct(int id){
+        int index = products.indexOf(getProduct(id));
+
+        return products.remove(index);
+    }
 }
