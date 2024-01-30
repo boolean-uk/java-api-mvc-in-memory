20e95fae794239bd5edf203a489b6202ae2c36ab
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-30 12:50:40 +0100

    Set up ProductRepository class

--- src/main/java/com/booleanuk/api/bagels/ProductRepository.java
+++ src/main/java/com/booleanuk/api/bagels/ProductRepository.java
@@ -1,4 +1,19 @@
 package com.booleanuk.api.bagels;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductRepository {
+    private int idCounter = 1;
+    private List<Product> products;
+
+
+    public ProductRepository() {
+        this.products = new ArrayList<>();
+
+        this.products.add(new Product(idCounter++, "Java", "Book", 1000));
+        this.products.add(new Product(idCounter++, "Onion Bagel", "Food", 1000));
+    }
+
+
 }
