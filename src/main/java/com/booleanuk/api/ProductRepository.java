5a8d4777af5cfce5875c992647a9670829397d4d
Marit <marit.moe2@ntebb.no>
2024-01-30 13:42:29 +0100

    Redundant initializer.

--- src/main/java/com/booleanuk/api/ProductRepository.java
+++ src/main/java/com/booleanuk/api/ProductRepository.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProductRepository {
-    private List<Product> products = new ArrayList<>();
+    private List<Product> products;
     
     public ProductRepository() {
         this.products = new ArrayList<>();
